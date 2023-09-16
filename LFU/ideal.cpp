8cff3bc3a3768319f2f03ae98eafab8e8a7255ab
Pelmeshka127 <kirya0703@gmail.com>
2023-09-16 22:44:49 +0300

    Added interactive input

--- LFU/ideal.cpp
+++ LFU/ideal.cpp
@@ -1,101 +1,144 @@
 #include "ideal.hpp"
 
-//==========================================================================================//
+int MakeConsoleInput(int argc, char **argv);
+
+int MakeFileInput(int argc, char **argv);
 
 using KeyT = int;
 KeyT slow_get_page(KeyT key) {return key;}
 
+//==========================================================================================//
+
 int main(int argc, char **argv)
 {
-    int mode = 0;
+    bool interactive = true;
 
-    std::cout << "Введите 1 для ввода данных с клавиатуры" << std::endl
-              << "Введите 2 для ввода данных с файла"      << std::endl;
-
-    std::cin >> mode;
-
-    if (mode == 1)
+    while (interactive)
     {
-        size_t my_capacity  = 0;
-        size_t values_count = 0;
+        std::cout   << "Введите 1 для ввода данных с клавиатуры" << std::endl
+                    << "Введите 2 для ввода данных с файла"      << std::endl;
 
-        std::cin >> my_capacity >> values_count;
+        int mode = 0;
 
-        std::vector<KeyT> array;
-        for (size_t i = 0; i != values_count; i++)
+        if (scanf("%d", &mode) == 1)
         {
-            int key = 0;
+            switch(mode)
+            {
+                case 1:
+                    MakeConsoleInput(argc, argv);
+                    interactive = false;
+                    break;
 
-            std::cin >> key;
-            
-            array.push_back(key);
+                case 2:
+                    MakeFileInput(argc, argv);
+                    interactive = false;
+                    break;
+                
+                default:
+                    std::cout << "Введено неверное число, повторите поппытку" << std::endl;
+                    while(getchar() != '\n')
+                        continue;
+            }
         }
 
-        ideal_cache::ideal_cache_t<int> my_cache(array, my_capacity);
-
-        const std::clock_t start = std::clock();
-
-        for (size_t i = 0; i < values_count; i++)
+        else
         {
-            int elem = 0;
-            std::cin >> elem;
-
-            my_cache.LookUpUpdate(elem, slow_get_page);
-            // my_cache.Dump();
+            std::cout << "Введен некорректный символ, повторите поппытку" << std::endl;
+            while(getchar() != '\n')
+                continue;
         }
-
-        std::cout << "Total hits are " << my_cache.GetHits() << std::endl;
-
-        std::cout << "Time is " << (std::clock() - start) / (double) CLOCKS_PER_SEC << std::endl;
     }
 
-    else if (mode == 2)
+    return 0;
+}
+
+//==========================================================================================//
+
+int MakeConsoleInput(int argc, char **argv)
+{
+    size_t my_capacity  = 0;
+    
+    size_t values_count = 0;
+
+    std::cin >> my_capacity >> values_count;
+
+    std::vector<KeyT> array;
+    for (size_t i = 0; i != values_count; i++)
     {
-        if (argc != 2)
-        {
-            std::cerr << "Incorrect number of command line arguments" << std::endl;
-            return -1;
-        }
+        int key = 0;
 
-        std::ifstream test_file{};
-
-        test_file.open(argv[1]);
-
-        size_t my_capacity  = 0;
-        size_t values_count = 0;
-
-        test_file >> my_capacity >> values_count;
-
-        std::vector<KeyT> array;
-        for (size_t i = 0; i != values_count; i++)
-        {
-            int key = 0;
-
-            test_file >> key;
-            
-            array.push_back(key);
-        }
-
-        ideal_cache::ideal_cache_t<int> my_cache(array, my_capacity);
-
-        // my_cache.DumpArray();
-
-        const std::clock_t start = std::clock();
-
-        for (auto elem : array)
-        {
-            my_cache.LookUpUpdate(elem, slow_get_page);
-
-            // my_cache.DumpCache();
-        }
-
-        std::cout << "Total hits are " << my_cache.GetHits() << std::endl;
-
-        std::cout << "Time is " << (std::clock() - start) / (double) CLOCKS_PER_SEC << std::endl;
-
-        test_file.close();
-
+        std::cin >> key;
+        
+        array.push_back(key);
     }
+
+    ideal_cache::ideal_cache_t<int> my_cache(array, my_capacity);
+
+    const std::clock_t start = std::clock();
+
+    for (size_t i = 0; i < values_count; i++)
+    {
+        int elem = 0;
+        std::cin >> elem;
+
+        my_cache.LookUpUpdate(elem, slow_get_page);
+        // my_cache.Dump();
+    }
+
+    std::cout << "Total hits are " << my_cache.GetHits() << std::endl;
+
+    std::cout << "Time is " << (std::clock() - start) / (double) CLOCKS_PER_SEC << std::endl;
+
+    return 0;   
+}
+
+//==========================================================================================//
+
+int MakeFileInput(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Incorrect number of command line arguments" << std::endl;
+        return -1;
+    }
+
+    std::ifstream test_file{};
+
+    test_file.open(argv[1]);
+
+    size_t my_capacity  = 0;
+    size_t values_count = 0;
+
+    test_file >> my_capacity >> values_count;
+
+    std::vector<KeyT> array;
+    for (size_t i = 0; i != values_count; i++)
+    {
+        int key = 0;
+
+        test_file >> key;
+        
+        array.push_back(key);
+    }
+
+    ideal_cache::ideal_cache_t<int> my_cache(array, my_capacity);
+
+    // my_cache.DumpArray();
+
+    const std::clock_t start = std::clock();
+
+    for (auto elem : array)
+    {
+        my_cache.LookUpUpdate(elem, slow_get_page);
+
+        // my_cache.DumpCache();
+    }
+
+    std::cout << "Total hits are " << my_cache.GetHits() << std::endl;
+
+    std::cout << "Time is " << (std::clock() - start) / (double) CLOCKS_PER_SEC << std::endl;
+
+    test_file.close();
 
     return 0;
 }
--- LFU/lfu.cpp
+++ LFU/lfu.cpp
@@ -1,79 +1,122 @@
 #include "lfu.hpp"
 
-//==========================================================================================//
-
 int slow_get_page (int key) {return key;}
+
+int MakeConsoleInput(int argc, char**argv);
+
+int MakeFileInput(int argc, char**argv);
+
+//==========================================================================================//
 
 int main(int argc, char **argv)
 {
-    int mode = 0;
+    bool interactive = true;
 
-    std::cout << "Введите 1 для ввода данных с клавиатуры" << std::endl
-              << "Введите 2 для ввода данных с файла"      << std::endl;
-
-    std::cin >> mode;
-
-    if (mode == 1)
+    while(interactive)
     {
-        size_t my_capacity  = 0;
-        size_t values_count = 0;
+        std::cout   << "Введите 1 для ввода данных с клавиатуры" << std::endl
+                    << "Введите 2 для ввода данных с файла"      << std::endl;
 
-        std::cin >> my_capacity >> values_count;
+        int mode = 0;
 
-        lfu::cache_t<int> my_cache(my_capacity);
-
-        const std::clock_t start = std::clock();
-        
-        for (size_t i = 0; i < values_count; i++)
+        if (scanf("%d", &mode) == 1)
         {
-            int elem = 0;
-            std::cin >> elem;
+            switch(mode)
+            {
+                case 1:
+                    MakeConsoleInput(argc, argv);
+                    interactive = false;
+                    break;
 
-            my_cache.LookUpUpdate(elem, slow_get_page);
-            // my_cache.Dump();
+                case 2:
+                    MakeFileInput(argc, argv);
+                    interactive = false;
+                    break;
+
+                default:
+                    std::cout << "Введено неверное число, повторите поппытку" << std::endl;
+                    while(getchar() != '\n')
+                        continue;
+            }
         }
 
-        std::cout << "Total hits are " << my_cache.GetHits() << std::endl;
-
-        std::cout << "Time is " << (std::clock() - start) / (double) CLOCKS_PER_SEC << std::endl;
-    }   
-
-    else if (mode == 2)
-    {
-        if (argc != 2)
+        else
         {
-            std::cerr << "Incorrect number of command line arguments" << std::endl;
-            return -1;
+            std::cout << "Введен некорректный символ, повторите поппытку" << std::endl;
+            while(getchar() != '\n')
+                continue;
         }
-        
-        std::ifstream test_file;
-
-        test_file.open(argv[1]);
-
-        size_t my_capacity  = 0;
-        size_t values_count = 0;
-
-        test_file >> my_capacity >> values_count;
-
-        lfu::cache_t<int> my_cache(my_capacity);
-
-        const std::clock_t start = std::clock();
-
-        for (size_t i = 0; i < values_count; i++)
-        {
-            int elem = 0;
-            test_file >> elem;
-
-            my_cache.LookUpUpdate(elem, slow_get_page);
-            // my_cache.Dump();
-        }
-
-        std::cout << "Total hits are " << my_cache.GetHits() << std::endl;
-
-        std::cout << "Time is " << (std::clock() - start) / (double) CLOCKS_PER_SEC << std::endl;
-        
-        test_file.close();
     }
+
+    return 0;
+}
+
+//==========================================================================================//
+
+int MakeConsoleInput(int argc, char **argv)
+{
+    size_t my_capacity  = 0;
+    size_t values_count = 0;
+
+    std::cin >> my_capacity >> values_count;
+
+    lfu::cache_t<int> my_cache(my_capacity);
+
+    const std::clock_t start = std::clock();
+
+    for (size_t i = 0; i < values_count; i++)
+    {
+        int elem = 0;
+        std::cin >> elem;
+
+        my_cache.LookUpUpdate(elem, slow_get_page);
+        // my_cache.Dump();
+    }
+
+    std::cout << "Total hits are " << my_cache.GetHits() << std::endl;
+
+    std::cout << "Time is " << (std::clock() - start) / (double) CLOCKS_PER_SEC << std::endl;
+
+    return 0;
+}
+
+//==========================================================================================//
+
+int MakeFileInput(int argc, char**argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Incorrect number of command line arguments" << std::endl;
+        return -1;
+    }
+    
+    std::ifstream test_file;
+
+    test_file.open(argv[1]);
+
+    size_t my_capacity  = 0;
+    size_t values_count = 0;
+
+    test_file >> my_capacity >> values_count;
+
+    lfu::cache_t<int> my_cache(my_capacity);
+
+    const std::clock_t start = std::clock();
+
+    for (size_t i = 0; i < values_count; i++)
+    {
+        int elem = 0;
+        test_file >> elem;
+
+        my_cache.LookUpUpdate(elem, slow_get_page);
+        // my_cache.Dump();
+    }
+
+    std::cout << "Total hits are " << my_cache.GetHits() << std::endl;
+
+    std::cout << "Time is " << (std::clock() - start) / (double) CLOCKS_PER_SEC << std::endl;
+    
+    test_file.close();
 
     return 0;
 }
